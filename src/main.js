261046499f045fbc1cb774c333323c550c796af7
Thekla921 <14959951+thekla921@user.noreply.gitee.com>
2025-06-27 13:08:39 +0000

    引入AOS动画库
    Signed-off-by: Thekla921 <14959951+thekla921@user.noreply.gitee.com>

--- src/main.js
+++ src/main.js
@@ -27,10 +27,19 @@ import './assets/css/style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createMetaManager } from 'vue-meta'
+// 引入AOS动画库
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 // 注释掉单独导入的ElCollapseTransition，因为ElementPlus已经包含它
 // import { ElCollapseTransition } from 'element-plus'
 // Vue.config.productionTip = false;
-
+// 初始化AOS
+AOS.init({
+  duration: 800,
+  easing: 'ease-in-out',
+  once: true,
+  offset: 100
+})
 const app = createApp(App)
 
 const metaManager = createMetaManager()
