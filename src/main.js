6d1159db106b6fa113c2284f04dcbc649e35b2ad
Thekla921 <14959951+thekla921@user.noreply.gitee.com>
2025-06-28 08:31:11 +0000

    update src/main.js.
    Signed-off-by: Thekla921 <14959951+thekla921@user.noreply.gitee.com>

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-//import { createApp } from 'vue'
-//import App from './App.vue'
-//import router from './routes/index'
-//import './assets/css/style.css'
-//import ElementPlus from 'element-plus'
-//import 'element-plus/dist/index.css'
-//import { createMetaManager } from 'vue-meta'
-//import { ElCollapseTransition } from 'element-plus'
-// Vue.config.productionTip = false;
-
-//const app = createApp(App)
-
-//const metaManager = createMetaManager()
-//app.use(metaManager)
-
-//app.component(ElCollapseTransition.name, ElCollapseTransition)
-//app.use(router)
-//app.use(ElementPlus)
-//app.mount('#app')
-
-
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index'
